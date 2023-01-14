624b3e5fabf8fa674e7673e6b775ed43d40af84e
EkaterinaKononenko <kaktysik4@ukr.net>
2023-01-14 14:00:59 +0200

    add markup for contact page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/auth/store';
+import { theme } from './components/theme';
+import { ThemeProvider } from 'styled-components';
+import 'modern-normalize';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/pages/Contacts.js
@@ -0,0 +1,40 @@
+//import { Filter } from 'components/Filter';
+//import { ContactForm } from 'components/ContactForm';
+//import { ContactList } from 'components/ContactList';
+import { Container, PhoneTitle, ContactTitle, IconWrapper } from './Contacts.styled';
+import { AiOutlineApple } from 'react-icons/ai';
+import { AiOutlineUsergroupAdd } from 'react-icons/ai';
+
+export default function Contacts() {
+  return (
+    <Container>
+      <IconWrapper>
+        <PhoneTitle>Phonebook</PhoneTitle>
+        <AiOutlineApple color="#D71868" fontSize="3em" />
+      </IconWrapper>
+      <div>Here will be markup</div>
+      <IconWrapper>
+        <ContactTitle>Contacts</ContactTitle>
+        <AiOutlineUsergroupAdd color="#D71868" fontSize="3em" />
+      </IconWrapper>
+      <div>Here will be markup</div>
+      <div>Here will be markup</div>
+    </Container>
+  );
+}
+
+/*return (
+    <Container>
+      <IconWrapper>
+        <PhoneTitle>Phonebook</PhoneTitle>
+        <AiOutlineApple color="#D71868" fontSize="3em" />
+      </IconWrapper>
+      <ContactForm />
+      <IconWrapper>
+        <ContactTitle>Contacts</ContactTitle>
+        <AiOutlineUsergroupAdd color="#D71868" fontSize="3em" />
+      </IconWrapper>
+      <Filter />
+      <ContactList />
+    </Container>
+  );*/
