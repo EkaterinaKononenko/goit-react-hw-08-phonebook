ccd9754397d04189e4f823e2cb0314cef885b8bf
EkaterinaKononenko <kaktysik4@ukr.net>
2023-01-15 12:35:28 +0200

    add userMenu style

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,21 +1,25 @@
 import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/operations';
 import { useAuth } from 'hooks';
-import css from './UserMenu.module.css';
+import { Box, Text, Button, Flex } from '@chakra-ui/react';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
 
   return (
-    <div className={css.wrapper}>
-      <div>
-        <p className={css.username}>Welcome, {user.name}</p>
-        <p>{user.email}</p>
-      </div>
-      <button type="button" onClick={() => dispatch(logOut())}>
+    <Flex as="div" alignItems="center" gap="12px">
+      <Box as="div" >
+        <Text as="p" fontWeight="700">
+          Welcome, {user.name}
+        </Text>
+        <Text as="p" fontWeight="700">
+          {user.email}
+        </Text>
+      </Box>
+      <Button as="button" type="button" onClick={() => dispatch(logOut())}>
         Logout
-      </button>
-    </div>
+      </Button>
+    </Flex>
   );
 };
